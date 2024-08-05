da57d9e21263b0035724de0a63b31aeb284e5f39
Zibeline <zibeline.dev@gmail.com>
2024-08-05 14:44:19 +0200

    💄 UI preparation for Ep 07

---
+++ components/button_collapse/button_collapse.gd
@@ -0,0 +1,25 @@
+class_name ButtonCollapse
+extends Button
+
+
+@export var _target : Control 
+
+
+func _ready() -> void :
+	pressed.connect(_on_pressed)
+	text = "+"
+	_target.visible = false
+
+
+func _on_pressed() -> void : 
+	_toggle_collapse()
+
+
+func _toggle_collapse() -> void : 
+	if _target.visible :
+		text = "+"
+		_target.visible = false
+	
+	else : 
+		text = "-"
+		_target.visible = true
